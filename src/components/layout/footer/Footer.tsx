dd7d185cce6acb971a3b73c235a334146d2058b4
aleksandr_fomin <a.fomin@prof-p.ru>
2023-12-19 08:39:45 +0300

    устранение eslint ошибок

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Typography } from "antd";
 import {FooterBlock, TextFooter} from "./styled";
 
 
 function Footer() {
     return (
-
       <FooterBlock>
             <TextFooter>Сделано в 2023г.</TextFooter>
         </FooterBlock>
